7c7d57c9a76655575bc84c7d11fb5a5c91e01783
Jamie Eula Evangelista <jevangelista@Jamies-MBP.att.net>
2015-08-29 14:55:43 -0700

    Got index.html to render

--- index.js
+++ index.js
@@ -12,17 +12,34 @@ var people = [
 	{name: "Beau", type:"boy"}
 ];
 
+//CONFIG//
+//serve js & css files
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
+// body parser config to accept all datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 //ROUTES
+
+//Render index.html route
+app.get("/", function (req, res){
+  // render index.html
+  res.sendFile(path.join(views + 'index.html'));
+});
 
 //Single route
 app.get("/", function (req, res) {
-	res.send("Hello Jamie!");
+	res.send("Hello there, Jamie!");
 });
+
+
 
 //Testing data route
 app.get("/people", function (req, res) {
 	res.send(people);
 });
+
 
 //SPECIFYING PORT
 app.listen(3000, function() {
